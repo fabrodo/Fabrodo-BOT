0977abd88aa564d65abf354535b7ff54fcb4fc3e
fabrodo <35458113+fabrodo@users.noreply.github.com>
2018-01-15 19:37:20 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,29 +1,27 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-
+ 
 client.on('ready', () => {
   console.log(`Bot ${client.user.tag} adı ile giriş yaptı!`);
 });
-
+ 
 client.on('message', msg => {
   if (msg.content === 'ping') {
     msg.reply('Pong!');
   }
-});
-
-client.on('message', msg => {
+ 
   if (msg.content.toLowerCase() === 'sa') {
     msg.reply('Aleyküm Selam :raised_hand:');
   }
-
-  client.on('message', msg => {
+ 
   if (msg.content.toLowerCase() === 'selamun aleyküm') {
     msg.reply('Aleyküm Selam :raised_hand:');
   }
  
-client.on('message', msg => {
   if (msg.content.toLowerCase() === 'sea') {
     msg.reply('Aleyküm Selam :raised_hand:');
   }
-  
+ 
+});
+ 
 client.login(process.env.BOT_TOKEN);
